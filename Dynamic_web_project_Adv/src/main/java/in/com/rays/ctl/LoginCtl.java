96acc7efb41642fa27f14bc25f5d66202b1db6ff
Azam khan <azamkhan5532@gmail.com>
2024-09-25 19:45:28 +0530

    initial commit

---
+++ Dynamic_web_project_Adv/src/main/java/in/com/rays/ctl/LoginCtl.java
@@ -0,0 +1,68 @@
+package in.com.rays.ctl;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import in.com.rays.bean.UserBean;
+import in.com.rays.model.UserModel;
+
+@WebServlet("/LoginCtl")
+public class LoginCtl extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String op = req.getParameter("operation");
+		if (op != null && op.equals("logout")) {
+			System.out.println("op >>>>"+op);
+			HttpSession session = req.getSession();
+			session.invalidate();
+		}
+		System.out.println("op else >>>>"+op);
+		resp.sendRedirect("LoginView.jsp");
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String loginId = req.getParameter("loginId");
+		String password = req.getParameter("password");
+		String op = req.getParameter("operation");
+		
+		HttpSession session = req.getSession();
+
+
+		if (op.equals("SignIn")) {
+			UserModel model = new UserModel();
+
+			try {
+				UserBean bean = model.authenticate(loginId, password);
+				if (bean != null) {
+					session.setAttribute("user", bean);
+					RequestDispatcher rd = req.getRequestDispatcher("Welcome.jsp");
+					rd.forward(req, resp);
+				} else {
+					req.setAttribute("msg", "login id & password invalid");
+					RequestDispatcher rd = req.getRequestDispatcher("LoginView.jsp");
+					rd.forward(req, resp);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		if (op.equals("SignUp")) {
+			resp.sendRedirect("UserRegistrationCtl");
+		}
+
+	}
+
+}
